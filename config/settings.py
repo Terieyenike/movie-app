652792b42417db4baf2889bc7a3fabcc49867097
Oteri Eyenike <teyenike1@gmail.com>
2024-06-23 23:15:18 +0100

    Fix: include staticfiles

--- config/settings.py
+++ config/settings.py
@@ -114,7 +114,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
-# STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = "staticfiles"
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # STATICFILES_DIRS = [
